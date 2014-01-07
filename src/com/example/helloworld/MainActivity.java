1c14fd3b60fba2a4fbb42133cdeeb8b984dd38e4
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-07 22:59:50 +0800

    ddd

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -12,13 +12,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        System.out.println("0000000000");
+        
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
 
     
 }
